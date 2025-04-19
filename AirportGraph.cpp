676de2b701ca1f6af53fb9af1a0a8526cc98f579
rtaqag35 <rtorresdeandrade570@floridapoly.edu>
2025-04-19 16:54:27 -0400

    Implementation of task 5

--- AirportGraph.cpp
+++ AirportGraph.cpp
@@ -75,3 +75,49 @@ void AirportGraph::displayGraph() const {
 const vector<Airport>& AirportGraph::getAirports() const {
     return airports;
 }
+
+//Counts and Sorts the connections of all airports. (could be optimized)
+void AirportGraph::displayConnections() const {
+    //array to store amount of connections
+    int connections[airports.size()] = { 0 };
+    //iterates through all connection add increments the array for both start and destination
+    for (const auto& airport : airports) {
+        
+        for (Edge* e = airport.head; e != nullptr; e = e->next) {
+            connections[airport.index]++;
+            connections[e->dest]++; 
+        }
+        
+    }
+    
+    struct AirportConnection {
+        string code;
+        int count;
+    };
+    
+    AirportConnection airportConnections[airports.size()];
+    
+    // Populate the array
+    for (const auto& airport : airports) {
+        airportConnections[airport.index].code = airport.code;
+        airportConnections[airport.index].count = connections[airport.index];
+    }
+    
+    // Sort the array in descending order based on connection count
+    for (size_t i = 0; i < airports.size() - 1; i++) {
+        for (size_t j = 0; j < airports.size() - i - 1; j++) {
+            if (airportConnections[j].count < airportConnections[j+1].count) {
+                // Swap
+                AirportConnection temp = airportConnections[j];
+                airportConnections[j] = airportConnections[j+1];
+                airportConnections[j+1] = temp;
+            }
+        }
+    }
+    
+    // Display the sorted connections
+    cout << "Airport connections:" << endl;
+    for (size_t i = 0; i < airports.size(); i++) {
+        cout << airportConnections[i].code << ": " << airportConnections[i].count << endl;
+    }
+}
