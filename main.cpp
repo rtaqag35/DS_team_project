c5275487e119c8e9cc223b681cb0e0931f7087dc
Ad3l31n3 <adomel555@gmail.com>
2025-04-20 15:36:51 -0400

    Made it so everything is tested

--- main.cpp
+++ main.cpp
@@ -6,12 +6,23 @@ using namespace std;
 int main() {
     AirportGraph graph;
     graph.loadTXT("airports.txt");
-
+    
     cout << "\n--- All Airports ---" << endl;
     graph.displayAirports();
 
     cout << "\n--- Airport Graph (Adjacency List) ---" << endl;
     graph.displayGraph();
 
+    graph.findShortestRoute("IAD", "MIA", true, true);
+    graph.findShortestRoute("PIT", "ACT", true, true);
+
+    graph.findAllShortestRoutesToState("ATL", "TX");
+    
+    graph.findShortestRouteGivenStops("IAD", "MIA", 3);
+    graph.findShortestRouteGivenStops("PIT", "ACT", 2);
+    
+    graph.displayConnections();
+    
+    graph.CreateGraphU();
     return 0;
 }
