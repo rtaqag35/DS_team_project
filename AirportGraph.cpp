9599577af3b5f1b5cf6ee0975e99173a555d5b00
rtaqag35 <rtorresdeandrade570@floridapoly.edu>
2025-04-20 20:27:54 -0400

    Added primMST and minKey

--- AirportGraph.cpp
+++ AirportGraph.cpp
@@ -358,3 +358,97 @@ void AirportGraph::CreateGraphU() const {
         
     }
 }
+
+int AirportGraph::minKey(vector<int> &key, vector<bool> &mstSet) const{
+  
+    // Initialize min value
+    int min = INT_MAX; 
+    int min_index = -1;
+
+    for (int v = 0; v < mstSet.size(); v++) {
+        if (mstSet[v] == false && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+//Creates Graph_U and the MST using Prim's algorithm
+void AirportGraph::primMST() const {
+    //Creates new array containing the new connections of GraphU
+    int newConnections[airports.size()][airports.size()] = { 0 };
+    for (const auto& airport : airports) {
+        for (Edge* e = airport.head; e != nullptr; e = e->next) {
+            if (newConnections[airport.index][e->dest] == 0) {
+                newConnections[airport.index][e->dest] = e->cost;
+                newConnections[e->dest][airport.index] = e->cost;
+            }
+            else if (newConnections[airport.index][e->dest] > e->cost) {
+                newConnections[airport.index][e->dest] = e->cost;
+                newConnections[e->dest][airport.index] = e->cost;
+            }
+        }
+        
+    }
+    
+    int V = airports.size();
+  
+    // Array to store constructed MST
+    vector<int> parent(V);
+
+    // Key values used to pick minimum weight edge in cut
+    vector<int> key(V);
+
+    // To represent set of vertices included in MST
+    vector<bool> mstSet(V);
+
+    // Initialize all keys as INFINITE
+    for (int i = 0; i < V; i++) {
+        key[i] = INT_MAX, mstSet[i] = false;
+    }
+
+    // Always include first 1st vertex in MST.
+    // Make key 0 so that this vertex is picked as first
+    // vertex.
+    key[0] = 0;
+  
+    // First node is always root of MST
+    parent[0] = -1;
+
+    // The MST will have V vertices
+    for (int count = 0; count < V - 1; count++) {
+        
+        // Pick the minimum key vertex from the
+        // set of vertices not yet included in MST
+        int u = minKey(key, mstSet);
+
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+
+        // Update key value and parent index of
+        // the adjacent vertices of the picked vertex.
+        // Consider only those vertices which are not
+        // yet included in MST
+        for (int v = 0; v < V; v++) {
+
+            // graph[u][v] is non zero only for adjacent
+            // vertices of m mstSet[v] is false for vertices
+            // not yet included in MST Update the key only
+            // if graph[u][v] is smaller than key[v]
+            if (newConnections[u][v] && mstSet[v] == false && newConnections[u][v] < key[v]) {
+                parent[v] = u, key[v] = newConnections[u][v];
+            }
+        }    
+    }
+    int total = 0;
+    cout << "Edge \tWeight\n";
+    for (int i = 1; i < airports.size(); i++) {
+        if (parent[i] >= 0 && parent[i] < V && i < V) {
+            cout << airports[parent[i]].code << " - " << airports[i].code << " \t" << newConnections[parent[i]][i] << " \n";
+            total += newConnections[parent[i]][i]; 
+        }
+    }
+    cout << "Total Cost of MST: " << total << endl;
+}
